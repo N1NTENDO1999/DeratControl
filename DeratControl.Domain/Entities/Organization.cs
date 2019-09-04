1f36f7bed0057833480a84a81786429abbb77a01
Mishka Sorochinskiy <mishash20161506@gmail.com>
2019-09-04 18:49:41 +0300

    Described Organization and Review Entities

--- DeratControl.Domain/Entities/Organization.cs
+++ DeratControl.Domain/Entities/Organization.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DeratControl.Domain.Root;
 
 namespace DeratControl.Domain.Entities
 {
-    class Organization
+    class Organization:EntityBase<int>
     {
+        public Organization()
+        {
+            this.Facilities = new List<Facility>();
+
+            this.ContactPeople = new List<User>();
+        }
+
+        public string Name { get; protected set; }
+
+        public ICollection<User> ContactPeople { get; protected set; }
+
+        public ICollection<Facility> Facilities { get; protected set; }
     }
 }
--- DeratControl.Domain/Entities/Review.cs
+++ DeratControl.Domain/Entities/Review.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DeratControl.Domain.Root;
 
 namespace DeratControl.Domain.Entities
 {
-    class Review
+    class Review:EntityBase<int>
     {
+        public Review()
+        {
+            this.ListOfTrapsToReview = new List<Trap>();
+        }
+
+        public DateTime Date { get; protected set; }
+
+        public DateTime StartedAt { get; protected set; }
+
+        public DateTime FinishedAt { get;protected set; }
+
+        public DateTime PostponedTo { get; protected set; }
+
+        public string Status { get; protected set; }
+
+        public Facility Facility { get; protected set; }
+
+        public User AssignedEmployee { get; protected set; }
+
+        public ICollection<Trap> ListOfTrapsToReview { get; protected set; }
     }
 }
