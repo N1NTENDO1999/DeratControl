f3be8c99112d9e685e5d16940cc00cf333830674
Nazarii Zahoroda <nazariizahoroda@gmail.com>
2019-09-04 19:40:42 +0300

    Added properties to Trap entity

--- DeratControl.Domain/Entities/Trap.cs
+++ DeratControl.Domain/Entities/Trap.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DeratControl.Domain.Root;
 
 namespace DeratControl.Domain.Entities
 {
-    class Trap
+    class Trap : EntityBase<int>
     {
+        public string Data { get; set; }
+        public virtual Point TrapPoint { get; set; }
+        public string TrapType { get; set; }
     }
 }
