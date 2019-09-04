1464396d6bf2d625b590685f2438a0ebb3142efa
Serhii Danylenko <2931190@gmail.com>
2019-09-04 15:17:36 +0300

    DomainExeption.cs cleared, EntityBase.cs set properties is protected now at LVXXXIVNET-2 task

--- DeratControl.Domain/Root/EntityBase.cs
+++ DeratControl.Domain/Root/EntityBase.cs
@@ -6,11 +6,10 @@ namespace DeratControl.Domain.Root
 {
     public abstract class EntityBase<TKey> where TKey : struct
     {
-        public virtual TKey Id { get; set; }
+        public virtual TKey Id { get; protected set; }
 
-        public virtual string CreatedBy { get; set; }
+        public virtual string CreatedBy { get; protected set; }
 
-        public virtual DateTime CreatedAt { get; set; }
+        public virtual DateTime CreatedAt { get; protected set; }
     }
 }
- 
