bb8bf18c159a62cf290deb75ead87ba0df73e668
Maria Vysotska <mariyavisotska@gmail.com>
2019-09-04 20:11:36 +0300

    Described Facility entity.

--- DeratControl.Domain/Entities/Facility.cs
+++ DeratControl.Domain/Entities/Facility.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace DeratControl.Domain.Entities
 {
-    class Facility
+    class Facility : Root.EntityBase<int>
     {
+        public string Address { get; set; }
+        public Organization Organization { get; set; }
+        public List<Perimeter> Perimeters { get; set; }
+        public List<Review> Reviews { get; set; }
+        public Facility()
+        {
+            this.Perimeters = new List<Perimeter>();
+            this.Reviews = new List<Review>();
+        }
     }
 }
