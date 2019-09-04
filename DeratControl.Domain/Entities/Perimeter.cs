f16a0c7efb51d0e353c5a05323f40f3b52475a42
Roman Voityk <voytik.roman21@gmail.com>
2019-09-04 18:28:49 +0300

    Added Perimeter Entity

--- DeratControl.Domain/Entities/Perimeter.cs
+++ DeratControl.Domain/Entities/Perimeter.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace DeratControl.Domain.Entities
 {
+    public enum PerimeterType
+    {
+        Internal,
+        External,
+        Third
+    }
     class Perimeter
     {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public User CreatedBy { get; set; }
+        public Facility Facility { get; set; }
+        public PerimeterType PerimeterType { get; set; }
+        public List<Point> TrapPoints { get; set; }
     }
 }
