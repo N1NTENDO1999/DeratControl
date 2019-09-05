be38669045c3f4612a8d51630f723c7e38f74ab7
Ihor <igorbilyk1970@gmail.com>
2019-09-05 20:58:57 +0300

    ReviewTrapHistoryEntity

--- DeratControl.Domain/Entities/TrapReview.cs
+++ DeratControl.Domain/Entities/TrapReview.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using DeratControl.Domain.Root;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
+
 
 namespace DeratControl.Domain.Entities
 {
-    class TrapReview
+    public enum TrapRewiewState
     {
+        Planned,
+        Active,
+        Done,
+        Postponed
+    }
+
+    class TrapReview : EntityBase<int>
+    {
+        Review Review { get; set; }
+        Point Point { get; set; }
+        string Comment { get; set; }
+        List<TrapReviewImage> ListOfImages { get; set; }
+        TrapRewiewState TrapRewiewState { get; set; }
+
+        public TrapReview()
+        {
+            ListOfImages = new List<TrapReviewImage>();
+        } 
     }
 }
