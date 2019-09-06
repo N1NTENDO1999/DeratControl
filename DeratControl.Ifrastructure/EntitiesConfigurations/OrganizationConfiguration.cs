a94c9f5027abf8a2a4d3e8889fcc3ad7b3f3d806
Mishka Sorochinskiy <mishash20161506@gmail.com>
2019-09-06 22:57:47 +0300

    Added config to Review and Organization. Registered config in DeratContext

--- DeratControl.Ifrastructure/EntitiesConfigurations/OrganizationConfiguration.cs
+++ DeratControl.Ifrastructure/EntitiesConfigurations/OrganizationConfiguration.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DeratControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DeratControl.Infrastructure.EntitiesConfigurations
 {
-    class OrganizationConfiguration
+    class OrganizationConfiguration : IEntityTypeConfiguration<Organization>
     {
+        public void Configure(EntityTypeBuilder<Organization> builder)
+        {
+            builder.HasKey(o => o.Id);
+
+            builder.Property(o => o.CreatedBy).HasMaxLength(40).IsRequired();
+
+            builder.HasMany(o => o.ContactPeople).WithOne(p=>p.Organization).OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(o => o.Facilities).WithOne(f=>f.Organization).OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
--- DeratControl.Ifrastructure/EntitiesConfigurations/ReviewConfiguration.cs
+++ DeratControl.Ifrastructure/EntitiesConfigurations/ReviewConfiguration.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using DeratControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DeratControl.Infrastructure.EntitiesConfigurations
 {
-    class ReviewConfiguration
+    class ReviewConfiguration : IEntityTypeConfiguration<Review>
     {
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+            builder.HasKey(r => r.Id);
+
+            builder.Property(r => r.CreatedBy).HasMaxLength(40).IsRequired();
+
+            builder.Property(r => r.Date).IsRequired();
+
+            builder.HasMany(o => o.ListOfTrapsToReview);
+        }
     }
 }
