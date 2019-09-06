cda49af9ac43df1921ecb751a17c950afa1b1edb
Mishka Sorochinskiy <mishash20161506@gmail.com>
2019-09-06 23:51:08 +0300

    Removed spaces

--- DeratControl.Ifrastructure/EntitiesConfigurations/BaseEntityConfiguration.cs
+++ DeratControl.Ifrastructure/EntitiesConfigurations/BaseEntityConfiguration.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DeratControl.Infrastructure.EntitiesConfigurations
 {
-   abstract class BaseEntityConfiguration<TEntity,TKey> : IEntityTypeConfiguration<TEntity> where TEntity : EntityBase<TKey>                                                                                            where TKey:struct
+   abstract class BaseEntityConfiguration<TEntity,TKey> : IEntityTypeConfiguration<TEntity> where TEntity : EntityBase<TKey> where TKey:struct
     {
         public virtual void Configure(EntityTypeBuilder<TEntity> builder)
         {
