d0b64a536fec11ebd65a5e6831837ac005a0c8cb
Maria Vysotska <mariyavisotska@gmail.com>
2019-09-06 13:07:52 +0300

    Changed access modifier in the constructor and in properties setter

--- DeratControl.Domain/Entities/Facility.cs
+++ DeratControl.Domain/Entities/Facility.cs
@@ -7,5 +7,14 @@ namespace DeratControl.Domain.Entities
 {
     public class Facility : EntityBase<int>
     {
+        public string Address { get; private set; }
+        public Organization Organization { get; private set; }
+        public List<Perimeter> Perimeters { get; private set; }
+        public List<Review> Reviews { get; private set; }
+        private Facility()
+        {
+            this.Perimeters = new List<Perimeter>();
+            this.Reviews = new List<Review>();
+        }
     }
 }
