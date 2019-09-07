55af9dec319a3b75ea180093ec03f7d1ad97641c
Roman Voityk <voytik.roman21@gmail.com>
2019-09-07 15:12:31 +0300

    Added middleware that will catch all exceptions and handle them. Also we can add our custmon exceptions and   return appropriate status code. Also added to project and configured Serilog

--- DeratControl.API/Startup.cs
+++ DeratControl.API/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DeratControl.API.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,6 +8,8 @@ namespace DeratControl.API
 {
     public class Startup
     {
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -23,8 +22,9 @@ namespace DeratControl.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseHttpStatusCodeExceptionMiddleware();
             }
-
+            app.UseHttpStatusCodeExceptionMiddleware();
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
