8d9d023d161fafd15c8edda5e262e22d2343a978
Maria Vysotska <mariyavisotska@gmail.com>
2019-09-08 16:34:20 +0300

    Added Facility Configuration Make collections and organization field virtual in Facility entity

--- DeratControl.Ifrastructure/EntitiesConfigurations/FacilityConfiguration.cs
+++ DeratControl.Ifrastructure/EntitiesConfigurations/FacilityConfiguration.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DeratControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DeratControl.Infrastructure.EntitiesConfigurations
 {
-    class FacilityConfiguration
+    class FacilityConfiguration : BaseEntityConfiguration<Facility, int>
     {
+        public override void Configure(EntityTypeBuilder<Facility> builder)
+        {
+            base.Configure(builder);
+
+            builder.HasMany(f => f.Perimeters).WithOne(p => p.Facility).OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(f => f.Reviews).WithOne(r => r.Facility).OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(f => f.Organization).WithMany(o => o.Facilities);
+        }
     }
 }
