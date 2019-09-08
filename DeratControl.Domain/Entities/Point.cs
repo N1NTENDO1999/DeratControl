5ab1f10e2ccc742093e4f1d62c684cb616fbcb3d
N1NTENDO1999 <denysnazarko99@gmail.com>
2019-09-08 13:40:04 +0300

    Add Point Fields

--- DeratControl.Domain/Entities/Point.cs
+++ DeratControl.Domain/Entities/Point.cs
@@ -7,5 +7,17 @@ namespace DeratControl.Domain.Entities
 {
     public class Point : EntityBase<int>
     {
+        private Point()
+        {
+            this.ListOfReviews = new HashSet<Review>();
+        }
+
+        public string Order { get; set; }
+        public virtual Perimeter Perimeter { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Review> ListOfReviews { get; protected set; }
+        public int TrapId { get; set; }
+
     }
 }
