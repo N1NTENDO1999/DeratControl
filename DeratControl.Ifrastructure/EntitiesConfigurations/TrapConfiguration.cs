234625441570af3877efe97a9eda34c4289295ee
Nazarii Zahoroda <nazariizahoroda@gmail.com>
2019-09-08 18:40:11 +0300

    TrapConfigure

--- DeratControl.Ifrastructure/EntitiesConfigurations/TrapConfiguration.cs
+++ DeratControl.Ifrastructure/EntitiesConfigurations/TrapConfiguration.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DeratControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DeratControl.Infrastructure.EntitiesConfigurations
 {
-    class TrapConfiguration
+    class TrapConfiguration: BaseEntityConfiguration<Trap,int>
     {
+        public override void Configure(EntityTypeBuilder<Trap> builder)
+        {
+            base.Configure(builder);
+        }
     }
 }
