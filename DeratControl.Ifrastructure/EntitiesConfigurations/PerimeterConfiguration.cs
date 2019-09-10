9a8ed982a0f27b7fe01a8140241a66ceee8ed9f0
Roman Voityk <voytik.roman21@gmail.com>
2019-09-10 08:06:40 +0300

    described perimeter entity configuration

--- DeratControl.Ifrastructure/EntitiesConfigurations/PerimeterConfiguration.cs
+++ DeratControl.Ifrastructure/EntitiesConfigurations/PerimeterConfiguration.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿using DeratControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
 namespace DeratControl.Infrastructure.EntitiesConfigurations
 {
-    class PerimeterConfiguration
+    class PerimeterConfiguration : BaseEntityConfiguration<Perimeter, int>
     {
+        public override void Configure(EntityTypeBuilder<Perimeter> builder)
+        {
+            base.Configure(builder);
+            builder.Property(p => p.PerimeterType).IsRequired();
+            builder.HasMany(p => p.TrapPoints).WithOne(t => t.Perimeter).OnDelete(DeleteBehavior.Cascade);
+            builder.HasOne(p => p.Facility).WithMany(f => f.Perimeters);
+        }
     }
 }
