e28fb3205d4fefc4875597edf65a1b4425583d44
Maria Vysotska <mariyavisotska@gmail.com>
2019-09-10 22:24:32 +0300

    Add OrganizationId to Facility

--- DeratControl.Domain/Entities/Facility.cs
+++ DeratControl.Domain/Entities/Facility.cs
@@ -8,6 +8,7 @@ namespace DeratControl.Domain.Entities
     public class Facility : EntityBase<int>
     {
         public string Address { get; private set; }
+        public int OrganizationId { get; private set; }
         public virtual Organization Organization { get; private set; }
         public virtual ICollection<Perimeter> Perimeters { get; private set; }
         public virtual ICollection<Review> Reviews { get; private set; }
