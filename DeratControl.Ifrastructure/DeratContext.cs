83e3deba1c3558c7ba7eff67f2ddf48f1de23b91
N1NTENDO1999 <denysnazarko99@gmail.com>
2019-09-12 16:27:10 +0300

    Add DbContext Implementation

--- DeratControl.Ifrastructure/DeratContext.cs
+++ DeratControl.Ifrastructure/DeratContext.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using DeratControl.Infrastructure.EntitiesConfigurations;
+using DeratControl.Domain.Entities;
 
 namespace DeratControl.Infrastructure
 {
-    public class DeratContext:DbContext
+    public class DeratContext : DbContext
     {
+
+        public DbSet<Facility> Facilities { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Perimeter> Perimeters { get; set; }
+        public DbSet<Point> Points { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Trap> Traps { get; set; }
+        public DbSet<TrapReview> TrapReviews { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DeratContext(DbContextOptions<DeratContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new OrganizationConfiguration());
             modelBuilder.ApplyConfiguration(new ReviewConfiguration());
-
             modelBuilder.ApplyConfiguration(new PointConfiguration());
             modelBuilder.ApplyConfiguration(new PerimeterConfiguration());
             modelBuilder.ApplyConfiguration(new UserConfiguration());
+
 
 
         }
