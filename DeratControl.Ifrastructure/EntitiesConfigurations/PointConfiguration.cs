bde6ecfb876d38bf6154fcff2a1f7a894508d6f5
N1NTENDO1999 <denysnazarko99@gmail.com>
2019-09-12 10:46:08 +0300

    Configure Foreign Keys

--- DeratControl.Ifrastructure/EntitiesConfigurations/PointConfiguration.cs
+++ DeratControl.Ifrastructure/EntitiesConfigurations/PointConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using DeratControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,10 @@ namespace DeratControl.Infrastructure.EntitiesConfigurations
             builder.Property(x => x.CreatedAt).IsRequired();
             builder.Property(x => x.CreatedBy).IsRequired();
             builder.Property(x => x.Order).HasMaxLength(255).IsRequired();
+
+            builder.HasMany(p => p.ListOfReviews).WithOne(t => t.Point).OnDelete(DeleteBehavior.Cascade);
+            builder.HasOne(p => p.Perimeter).WithMany(f => f.TrapPoints).HasForeignKey(p => p.PerimeterId);
+            builder.HasOne(p => p.Trap).WithOne(f => f.TrapPoint).HasForeignKey<Point>(e => e.TrapId);
         }
     }
 }
