e53124a966d89ba6e3d3eb137a90d6907be31aff
Роман Войтик <voytik.roman21@gmail.com>
2019-09-15 11:42:50 +0300

    hot fix

--- DeratControl.Domain/Root/Repositories/IFacilityRepository.cs
+++ DeratControl.Domain/Root/Repositories/IFacilityRepository.cs
@@ -6,6 +6,5 @@ namespace DeratControl.Domain.Root.Repositories
 {
     public interface IFacilityRepository : IRepository<Facility, int>
     {
-        Task<bool> IsInclude(int facilityId, PerimeterType perimeterType);
     }
 }
