3dcaeaa45d921a547964823281a29caef04a6d0a
Роман Войтик <voytik.roman21@gmail.com>
2019-09-16 16:07:03 +0300

    hot fix

--- DeratControl.Application/Perimeters/Commands/AddPerimeterCommand.cs
+++ DeratControl.Application/Perimeters/Commands/AddPerimeterCommand.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using DeratControl.Application.Interfaces;
 using DeratControl.Application.Requests;
 using DeratControl.Application.Requests.Interfaces;
 using DeratControl.Domain.Entities;
-using DeratControl.Domain.Root.Repositories;
-using DeratControl.Domain.Root.Exceptions;
+using DeratControl.Domain.Root;
 using System.Linq;
-
 namespace DeratControl.Application.Perimeters.Commands
 {   /// <summary>
 /// DTO class for adding perimeters command
@@ -24,26 +20,21 @@ namespace DeratControl.Application.Perimeters.Commands
     /// </summary>
     public class AddPerimeterCommandHandler : BaseCommandHandler<AddPerimeterCommand>
     {
-        private readonly IFacilityRepository _repository;
-        //TODO : inject IUnitOfWork instead of IFacilityRepository when available
-        public AddPerimeterCommandHandler(IFacilityRepository repository)
+        private readonly IUnitOfWork unitOfWork;
+        public AddPerimeterCommandHandler(IUnitOfWork unitOfWork)
         {
-            this._repository = repository;
+            this.unitOfWork = unitOfWork;
         }
         protected override async Task<CommandResult> HandleRequest(CommandExecutionContext executionContext, AddPerimeterCommand request)
         {
 
-            var facility = _repository.FindById(request.FacilityId);
+            var facility = unitOfWork.FacilityRepository.FindById(request.FacilityId);
             if (facility == null)
                 throw new Exception("this facility doesn`t exists");
             facility.AddPerimeter(request.PerimeterType, executionContext.RequestedUser);
-            //_repository.Save();//Unit Of Work will do it instead repository
+            unitOfWork.Commit();
             var id = from i in facility.Perimeters where i.PerimeterType == request.PerimeterType select i.Id;
             return new CommandCreateResult<int>(id.First());
-        }
-        protected override void AssertRequestIsValid(AddPerimeterCommand request)
-        {
-            base.AssertRequestIsValid(request);
         }
     }
 }
