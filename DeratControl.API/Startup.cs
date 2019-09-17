13f38dbd864d984ce4bba9b52a7c60de156c500d
bogdanpetrovsky <bpetrovsky977@gmail.com>
2019-09-17 21:52:06 +0300

    Added implementation for Organization Method Exists

--- DeratControl.API/Startup.cs
+++ DeratControl.API/Startup.cs
@@ -76,7 +76,7 @@ namespace DeratControl.API
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                    //c.RoutePrefix = string.Empty;
+                    c.RoutePrefix = string.Empty;
                 });
             }
             app.UseHttpStatusCodeExceptionMiddleware();
