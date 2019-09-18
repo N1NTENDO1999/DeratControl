95c022d38f74b5a269ad81b964b6c207ef736a91
Роман Войтик <voytik.roman21@gmail.com>
2019-09-18 21:25:29 +0300

    Remove perimeter endpoint with changes

--- DeratControl.Domain/Entities/Facility.cs
+++ DeratControl.Domain/Entities/Facility.cs
@@ -1,6 +1,8 @@
 ﻿using DeratControl.Domain.Root;
+using DeratControl.Domain.Root.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DeratControl.Domain.Entities
@@ -12,8 +14,41 @@ namespace DeratControl.Domain.Entities
         public virtual Organization Organization { get; private set; }
         public virtual ICollection<Perimeter> Perimeters { get; private set; } = new HashSet<Perimeter>();
         public virtual ICollection<Review> Reviews { get; private set; } = new HashSet<Review>();
+
         private Facility()
         {
+        }
+
+        public void RemovePerimeter(PerimeterType perimeterType)
+        {
+            var isPerimeterExists = this.Perimeters.Any(f => f.PerimeterType == perimeterType);
+            if (isPerimeterExists == false)
+            {
+                throw new PerimeterDoesNotExistException();
+            }
+
+            var perimeterToRemove = (from perimeter in this.Perimeters
+                                     where perimeter.PerimeterType == perimeterType
+                                     select perimeter).SingleOrDefault();
+
+            if (perimeterToRemove == null)
+            {
+                throw new PerimeterDoesNotExistException();
+            }
+
+            var isHasActiveReview = (from point in perimeterToRemove.TrapPoints
+                                     from t in point.ListOfReviews
+                                     select t).Any(t => t.TrapRewiewState == TrapRewiewState.Active);
+
+            if (isHasActiveReview)
+            {
+                throw new PerimeterHasActiveReviewException();
+            }
+
+            else
+            {
+                this.Perimeters.Remove(perimeterToRemove);
+            }
         }
     }
 }
