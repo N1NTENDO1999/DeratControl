2c4ad26f4ce8f6a51c8479834e7a5c1d4af60d6a
bogdanpetrovsky <bpetrovsky977@gmail.com>
2019-09-19 01:30:07 +0300

    Added Endpoint for getting Users through Employee

---
+++ DeratControl.Application/Review/Queries/GetReviewsByEmployee/GetReviewsQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using DeratControl.Application.Interfaces;
+using DeratControl.Application.Requests.Interfaces;
+using DeratControl.Domain.Root;
+using DeratControl.Domain.Root.Exceptions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeratControl.Application.Review.Queries.GetReviewsByEmpleyee
+{
+    class GetReviewsQueryHandler : IQueryHandler<GetReviewsQuery, ReviewsDTO>
+    {
+        private  readonly IUnitOfWork unitOfWork;
+
+        public GetReviewsQueryHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<ReviewsDTO> Handle(CommandExecutionContext executionContext, GetReviewsQuery request)
+        {
+            var employee = unitOfWork.UserRepository.FindByIdAsync(request.EmployeeId);
+
+            if (employee.Result.OrganizationId == null)
+                throw new UserIsNotEmployeeException();
+
+            var review = employee.Result.Reviews;
+
+            return (new ReviewsDTO(review));
+        }
+    }
+}
