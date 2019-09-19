34080b6cacbe0e6e58c80f6e294e64d83c447830
Роман Войтик <voytik.roman21@gmail.com>
2019-09-19 15:50:15 +0300

    endpoint to get perimeter individual

---
+++ DeratControl.Application/Perimeters/Queries/GetPerimeterIndividual/GetPerimeterIndividualQuery.cs
@@ -0,0 +1,36 @@
+﻿using DeratControl.Application.Interfaces;
+using DeratControl.Application.Requests.Interfaces;
+using DeratControl.Domain.Root;
+using DeratControl.Domain.Root.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeratControl.Application.Perimeters.Queries.GetPerimeterIndividual
+{
+    public class GetPerimeterIndividualQuery : IRequest
+    {
+        public int PerimeterId { get; set; }
+    }
+
+    public class GetPerimeterIndividualQueryHandler : IQueryHandler<GetPerimeterIndividualQuery, PerimeterIndividualViewModel>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public GetPerimeterIndividualQueryHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<PerimeterIndividualViewModel> Handle(CommandExecutionContext executionContext, GetPerimeterIndividualQuery request)
+        {
+            var perimeter = await unitOfWork.PerimeterRepository.FindByIdAsync(request.PerimeterId);
+            if (perimeter == null)
+            {
+                throw new PerimeterDoesExistsException();
+            }
+
+            return new PerimeterIndividualViewModel(perimeter.Facility, perimeter.PerimeterType, perimeter.TrapPoints);
+        }
+    }
+}
---
+++ DeratControl.Application/Perimeters/Queries/GetPerimeterIndividual/PerimeterIndividualViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DeratControl.Application.Requests.Interfaces;
+using DeratControl.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeratControl.Application.Perimeters.Queries.GetPerimeterIndividual
+{
+    public class PerimeterIndividualViewModel : IQueryResult
+    {
+
+        public virtual Facility Facility { get; set; }
+
+        public PerimeterType PerimeterType { get; set; }
+
+        public virtual ICollection<Point> TrapPoints { get; protected set; } = new HashSet<Point>();
+
+        public PerimeterIndividualViewModel(Facility facility, PerimeterType perimeterType, ICollection<Point> trapPoints)
+        {
+            this.Facility = facility;
+            this.PerimeterType = perimeterType;
+            this.TrapPoints = trapPoints;
+        }
+    }
+}
