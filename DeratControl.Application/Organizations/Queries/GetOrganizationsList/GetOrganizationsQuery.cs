4d2fcbc315c3e3678f7913fbbde78808fa30cd9d
Serhii Danylenko <2931190@gmail.com>
2019-09-19 15:51:32 +0300

    addedquery

---
+++ DeratControl.Application/Organizations/Queries/GetOrganizationsList/GetOrganizationsQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DeratControl.Application.Interfaces;
+using DeratControl.Domain.Root;
+
+namespace DeratControl.Application.Organizations.Queries.GetOrganizationsList
+{
+    public class GetOrganizationsQuery : IRequest
+    {
+        public int OrganizationID { get; set; }
+    }
+
+
+}
