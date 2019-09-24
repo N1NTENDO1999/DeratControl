9542a2e34a77fffbbb5da807496c9613d45efb02
Serhii Danylenko <2931190@gmail.com>
2019-09-24 17:30:16 +0300

    view organizations

--- DeratControl.Application/Organizations/Queries/GetOrganizationsList/GetOrganizationsQuery.cs
+++ DeratControl.Application/Organizations/Queries/GetOrganizationsList/GetOrganizationsQuery.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using DeratControl.Application.Interfaces;
+using DeratControl.Application.Requests.Interfaces;
+using DeratControl.Domain.Entities;
 using DeratControl.Domain.Root;
+using DeratControl.Domain.Root.Exceptions;
 
 namespace DeratControl.Application.Organizations.Queries.GetOrganizationsList
 {
@@ -11,5 +15,23 @@ namespace DeratControl.Application.Organizations.Queries.GetOrganizationsList
         public int OrganizationID { get; set; }
     }
 
+    public class GetOrganizationsQueryHandler : IQueryHandler<GetOrganizationsQuery, OrganizationsViewModelResult>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public GetOrganizationsQueryHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<OrganizationsViewModelResult> Handle(CommandExecutionContext executionContext, GetOrganizationsQuery request)
+        {
+            ICollection<Organization> organizations = (ICollection<Organization>)unitOfWork.OrganizationRepository.List;
+            if (organizations == null)
+            {
+                throw new OrganizationNotExistException();
+            }
+            return await Task.FromResult(new OrganizationsViewModelResult() { Organizations = organizations });
+        }
+    }
 
 }
